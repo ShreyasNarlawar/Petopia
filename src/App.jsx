9e852eea9ea3053009f1d9e6ba46715fc65bae81
mitali <patilmitalisur@gmail.com>
2024-12-31 00:35:51 +0530

    Add [description of changes]

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css'; // Import the CSS file for styling
+import Blog from './Component/Blog'; // Import the Blog component
+
+function App() {
+  return (
+    <div className="App">
+      {/* Display the Blog page */}
+      <Blog />
+    </div>
+  );
+}
+
+export default App;
